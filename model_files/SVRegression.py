71b58adcc4edc5861bc23664e37ff086836ee2eb
Codeharbor-01 <aadritthapa01@gmail.com>
2026-09-24 19:52:29 +0545

    Retrained SVR model with reduced training time

--- model_files/SVRegression.py
+++ model_files/SVRegression.py
@@ -2,7 +2,7 @@ import pandas as pd
 import joblib
 
 from sklearn.metrics import r2_score,mean_absolute_error,root_mean_squared_error
-from sklearn.preprocessing import StandardScaler,OrdinalEncoder
+from sklearn.preprocessing import StandardScaler
 from category_encoders import BinaryEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
@@ -10,24 +10,30 @@ from sklearn.pipeline import Pipeline
 from sklearn.svm import SVR
 from sklearn.model_selection import GridSearchCV
 
-df = pd.read_csv(r'C:\Users\Asus\Desktop\Projects\Dataset\cleaned_student_data.csv')
+df = pd.read_csv(r"C:\Users\Asus\Desktop\Projects\Dataset\cleaned_student_data.csv")
 
-x = df.drop(columns=['student_id','final_exam_score','final_grade'])
-y = df['final_exam_score']
+x = df.drop(columns=['Student_ID','Final_CGPA'])
+y = df['Final_CGPA']
 
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+x_train,x_test,y_train,y_test = train_test_split(x,y,random_state=42,test_size=0.2)
 
-num_columns = ['study_time_hours','attendance_percent','sleep_hours','previous_grade']
-ord_columns = ['parental_education']
-nom_columns = ['gender']
-bool_columns = ['internet_access','extracurricular_activities','part_time_job']
+num_columns = [
+    'Age',
+    'Attendance_Pct',
+    'Study_Hours_Per_Day',
+    'Previous_CGPA',
+    'Sleep_Hours',
+    'Social_Hours_Week'
+]
+nom_columns = [
+    'Gender',
+    'Major'
+]
 
-education_order = ['High School','Bachelors','Masters','PhD']
 
 preprocessor = ColumnTransformer(
     transformers=[
         ('num_scaled',StandardScaler(),num_columns),
-        ('ord_encoded',OrdinalEncoder(categories=[education_order]),ord_columns),
         ('nom_encoded',BinaryEncoder(),nom_columns)
     ],remainder='passthrough'
 )
@@ -40,11 +46,12 @@ pipe = Pipeline(
 )
 
 params = {
-    'regressor__C':range(1,21),
-    'regressor__kernel':['linear','rbf','poly']
+    'regressor__C':[0.1,1,10,100],
+    'regressor__kernel':['linear','rbf'],
+    'regressor__gamma': ['scale', 'auto']
 }
 
-grid_model = GridSearchCV(pipe,param_grid=params,cv=10,scoring='r2')
+grid_model = GridSearchCV(pipe,param_grid=params,cv=5,scoring='r2',n_jobs=-1)
 grid_model.fit(x_train,y_train)
 
 best_model = grid_model.best_estimator_
