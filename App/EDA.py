1fcfbc0cf047bfff284dc344da424bf3eed2d5ee
Codeharbor-01 <aadritthapa01@gmail.com>
2026-09-24 17:58:16 +0545

    Modified EDA to fit new dataset

--- App/EDA.py
+++ App/EDA.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
-uncleaned_file_path = r'C:\Users\Asus\Desktop\Projects\Dataset\student_performance_dataset.csv'
+uncleaned_file_path = r'C:\Users\Asus\Desktop\Projects\Dataset\Student_data.csv'
 uncleaned_df = pd.read_csv(uncleaned_file_path)
 
 cleaned_file_path = r'C:\Users\Asus\Desktop\Projects\Dataset\cleaned_student_data.csv'
@@ -146,93 +146,93 @@ st.markdown("""
 
 """)
 fig1,ax = plt.subplots()
-tab2.subheader('Gender vs Final Exam Score')
-gender_avg = df.groupby('gender').agg(
-        Average_Final_Exam_Score = ('final_exam_score','mean')
+tab2.subheader('Gender vs Final CGPA')
+gender_avg = df.groupby('Gender').agg(
+        Average_Final_CGPA = ('Final_CGPA','mean')
     ).reset_index()
 
 tab2.dataframe(gender_avg.round(2),hide_index=True)
 
-ax.set_title('Gender vs Final Exam Score')
+ax.set_title('Gender vs Final CGPA')
 ax.set_xlabel('Gender')
-ax.set_ylabel('Avg Final Exam Score')
-sns.barplot(gender_avg,x='gender',y='Average_Final_Exam_Score',ax=ax)
+ax.set_ylabel('Avg Final CGPA')
+sns.barplot(gender_avg,x='Gender',y='Average_Final_CGPA',ax=ax)
 tab2.pyplot(fig1)
 tab2.divider()
 
-tab2.subheader('Parental Education vs Final Exam Score')
-parental_edu_avg =  df.groupby('parental_education').agg(
-        Average_Final_Exam_Score = ('final_exam_score','mean')
+tab2.subheader('Attendance Percentage vs Final CGPA')
+attendance_cgpa_avg =  df.groupby('Attendance_Pct').agg(
+        Average_Final_CGPA = ('Final_CGPA','mean')
     ).reset_index()
 
-tab2.dataframe(parental_edu_avg.round(2),hide_index=True)
+tab2.dataframe(attendance_cgpa_avg.round(2),hide_index=True)
 
 fig2,ax = plt.subplots()
-ax.set_title('Parental Education vs Final Exam Score')
-ax.set_xlabel('Parental Education')
+ax.set_title('Attendance Percentage vs Final CGPA')
+ax.set_xlabel('Attendance Percent')
 ax.set_ylabel('Final Exam Score')
-sns.barplot(parental_edu_avg,x='parental_education',y='Average_Final_Exam_Score',ax=ax)
+sns.scatterplot(attendance_cgpa_avg,x='Attendance_Pct',y='Average_Final_CGPA',ax=ax)
 tab2.pyplot(fig2)
 tab2.divider()
 
-tab2.subheader('Comparison of Study time and Final Exam Score')
-study_and_avg_final_score = df.groupby(['study_time_hours']).agg(
-       Average_final_exam_score = ('final_exam_score','mean')
+tab2.subheader('Comparison of Study time and Final CGPA')
+study_and_avg_final_CGPA = df.groupby(['Study_Hours_Per_Day']).agg(
+       Average_final_CGPA = ('Final_CGPA','mean')
     ).reset_index()
 
-tab2.dataframe(study_and_avg_final_score.round(2),hide_index=True)
+tab2.dataframe(study_and_avg_final_CGPA.round(2),hide_index=True)
 
 fig3,ax = plt.subplots()
-ax.set_title('Study Time vs Final Exam Score')
+ax.set_title('Study Time vs Final CGPA')
 ax.set_xlabel('Study Time (in hours)')
-ax.set_ylabel('Average Final Exam Score')
-sns.scatterplot(study_and_avg_final_score,x='study_time_hours',y='Average_final_exam_score',ax=ax)
+ax.set_ylabel('Average Final CGPA')
+sns.scatterplot(study_and_avg_final_CGPA,x='Study_Hours_Per_Day',y='Average_final_CGPA',ax=ax)
 tab2.pyplot(fig3)
 tab2.divider()
 
-tab2.subheader('Impact of Attendance on Final Exam Score')
-tab2.dataframe(
-    df.groupby('attendance_percent')['final_exam_score'].mean()
-)
+# tab2.subheader('Impact of Attendance on Final Exam Score')
+# tab2.dataframe(
+#     df.groupby('attendance_percent')['final_exam_score'].mean()
+# )
 
-fig4,ax = plt.subplots()
-ax.set_title('Attendance Percent vs Final Exam Score')
-ax.set_xlabel('Attendance Percent')
-ax.set_ylabel('Average Final Exam Score')
-sns.scatterplot(df,x='attendance_percent',y='final_exam_score',ax=ax)
-tab2.pyplot(fig4)
-tab2.divider()
+# fig4,ax = plt.subplots()
+# ax.set_title('Attendance Percent vs Final Exam Score')
+# ax.set_xlabel('Attendance Percent')
+# ax.set_ylabel('Average Final Exam Score')
+# sns.scatterplot(df,x='attendance_percent',y='final_exam_score',ax=ax)
+# tab2.pyplot(fig4)
+# tab2.divider()
 
 tab2.subheader('Previous Grade vs Final Exam Score')
-avg_prev_vs_avg_final_grade = df.groupby('study_time_hours').agg(
-        Average_Previous_Grade = ('previous_grade','mean'),
-        Average_Final_Exam_Score = ('final_exam_score','mean')
-    ).sort_values(by='study_time_hours',ascending=True)
-tab2.dataframe(avg_prev_vs_avg_final_grade.round(2),hide_index=True)
+avg_prev_vs_avg_final_CGPA = df.groupby('Study_Hours_Per_Day').agg(
+        Average_Previous_CGPA = ('Previous_CGPA','mean'),
+        Average_Final_CGPA = ('Final_CGPA','mean')
+    ).sort_values(by='Study_Hours_Per_Day',ascending=True)
+tab2.dataframe(avg_prev_vs_avg_final_CGPA.round(2),hide_index=True)
 
 fig5,ax = plt.subplots(2,1)
 
 ax[0].set_title('Trend of study time and final exam score')
 ax[0].set_xlabel('Study Time (in hours)')
-ax[0].set_ylabel('Average Final Exam Score')
-sns.lineplot(avg_prev_vs_avg_final_grade,x='study_time_hours',y='Average_Final_Exam_Score',ax=ax[0])
+ax[0].set_ylabel('Average Final CGPA')
+sns.lineplot(avg_prev_vs_avg_final_CGPA,x='Study_Hours_Per_Day',y='Average_Final_CGPA',ax=ax[0])
 
 ax[1].set_title('Trend of study time and previous grade')
 ax[1].set_xlabel('Study Time (in hours)')
-ax[1].set_ylabel('Average Previous Grade')
-sns.lineplot(avg_prev_vs_avg_final_grade,x='study_time_hours',y='Average_Previous_Grade',ax=ax[1])
+ax[1].set_ylabel('Average Previous CGPA')
+sns.lineplot(avg_prev_vs_avg_final_CGPA,x='Study_Hours_Per_Day',y='Average_Previous_CGPA',ax=ax[1])
 plt.tight_layout()
 plt.subplots_adjust(hspace=1)
 tab2.pyplot(fig5)
 tab2.divider()
-tab2.subheader('Gender vs Internet Access')
+tab2.subheader('Gender-wise Field Enrollment')
 
-gender_vs_internet_access = df.groupby(['gender','internet_access']).size().reset_index(name='Number of Students')
+gender_subject_enrollment = df.groupby(['Gender','Major']).size().reset_index(name='Number of Students')
 
-tab2.dataframe(gender_vs_internet_access,hide_index=True)
+tab2.dataframe(gender_subject_enrollment,hide_index=True)
 fig6,ax = plt.subplots()
 ax.set_xlabel('Gender')
-sns.barplot(gender_vs_internet_access,x='gender',y='Number of Students',hue='internet_access')
+sns.barplot(gender_subject_enrollment,x='Gender',y='Number of Students',hue='Major')
 tab2.pyplot(fig6)
 tab2.divider()
 
@@ -244,11 +244,11 @@ sns.heatmap(numerical_cols,annot=True,cmap='coolwarm',fmt='.2f',ax=ax)
 tab2.pyplot(fig7)
 tab2.divider()
 
-tab2.subheader('Final Exam Score Distribution')
+tab2.subheader('Final CGPA Distribution')
 fig8,ax = plt.subplots()
-sns.histplot(df,x='final_exam_score',bins=10,kde=True,ax=ax)
+sns.histplot(df,x='Final_CGPA',bins=10,kde=True,ax=ax)
 
-ax.set_xlabel('Final Exam Score')
+ax.set_xlabel('Final Exam CGPA')
 ax.set_ylabel('Number of students')
 tab2.pyplot(fig8)
 
@@ -262,23 +262,23 @@ box = tab3.container(border=True)
 c1,c2,c3 = box.columns(3)
         
 c1.metric(
-    "Average Final Exam Score",
-    df['final_exam_score'].mean().round(2),
-    (df['final_exam_score'].mean()-df['previous_grade'].mean()).round(2),
+    "Average Final CGPA",
+    df['Final_CGPA'].mean().round(2),
+    (df['Final_CGPA'].mean()-df['Previous_CGPA'].mean()).round(2),
     delta_color='normal'
 )
 
 c2.metric(
     "Average Previous Grade",
-    df['previous_grade'].mean().round(2),
-    abs((df['previous_grade'].mean()-df['final_exam_score'].mean()).round(2)),
+    df['Previous_CGPA'].mean().round(2),
+    abs((df['Previous_CGPA'].mean()-df['Final_CGPA'].mean()).round(2)),
     delta_arrow='down',
     delta_color='red'
 )
 
 c3.metric(
     "Average Attendance Percent",
-    f"{df['attendance_percent'].mean().round(2)}%",
+    f"{df['Attendance_Pct'].mean().round(2)}%",
     'High Attendance',
     delta_arrow='off',
     delta_color='blue'
@@ -289,28 +289,28 @@ tab3.markdown(f"""
     <table>
         <tr>
             <th>Measure</th>
-            <th>Previous Grade</th>
-            <th>Final Exam Score</th>
+            <th>Previous CGPA</th>
+            <th>Final CGPA</th>
         </tr>
         <tr>
             <th>Mean</th>
-            <td>{df['previous_grade'].mean().round(2)}</td>
-            <td>{df['final_exam_score'].mean().round(2)}</td>
+            <td>{df['Previous_CGPA'].mean().round(2)}</td>
+            <td>{df['Final_CGPA'].mean().round(2)}</td>
         </tr>
         <tr>
             <th>Minimum</th>
-            <td>{df['previous_grade'].min()}</td>
-            <td>{df['final_exam_score'].min()}</td>
+            <td>{df['Previous_CGPA'].min()}</td>
+            <td>{df['Final_CGPA'].min()}</td>
         </tr>
         <tr>
             <th>Maximum</th>
-            <td>{df['previous_grade'].max()}</td>
-            <td>{df['final_exam_score'].max()}</td>
+            <td>{df['Previous_CGPA'].max()}</td>
+            <td>{df['Final_CGPA'].max()}</td>
         </tr>
         <tr>
             <th>Standard Deviation</th>
-            <td>{df['previous_grade'].std().round(2)}</td>
-            <td>{df['final_exam_score'].std().round(2)}</td>
+            <td>{df['Previous_CGPA'].std().round(2)}</td>
+            <td>{df['Final_CGPA'].std().round(2)}</td>
         </tr>
     </table>
 </div>
@@ -324,12 +324,13 @@ tab3.markdown(f"""
 tab3.divider()
 
 tab3.subheader('2. Student Background Summary')
-tab3.markdown('The table below shows the count of students with **Internet Access** and involved in **Part time job** and **Extracurricular activites**.')
+tab3.markdown('The table below shows the count of students with their **enrollment hours** in different activities.')
 performance_table = {
-    'Values':['True','False'],
-    'Internet Access':df['internet_access'].value_counts(),
-    'Part-Time Job':df['part_time_job'].value_counts(),
-    'Extracurricular Activities':df['extracurricular_activities'].value_counts()
+   'Average Attendance Percentage':df.groupby('Major')['Attendance_Pct'].mean().round(2),
+   'Average Study Hours':df.groupby('Major')['Study_Hours_Per_Day'].mean().round(2),
+   'Average Social Hours':df.groupby('Major')['Social_Hours_Week'].mean().round(2),
+   'Average Previous CGPA':df.groupby('Major')['Previous_CGPA'].mean().round(2),
+   'Average Final CGPA':df.groupby('Major')['Final_CGPA'].mean().round(2)
 }
 
 tab3.dataframe(performance_table)
@@ -345,9 +346,9 @@ tab3.divider()
 tab3.subheader('Conclusion')
 tab3.markdown("" \
     "The exploratory data analysis provided useful insights into student academic performance. " \
-    "**Previous grades, study time, and attendance** showed meaningful relationships with **final exam scores**, " \
+    "**Previous grades, study time, and attendance** showed meaningful relationships with **Final CGPA**, " \
     "while other student characteristics provided additional context. The analysis also confirmed that" \
     " the dataset is suitable for further machine learning analysis. Overall, these findings help identify" \
-    " relevant features that can be used to develop and evaluate models for predicting students' final exam scores.")
+    " relevant features that can be used to develop and evaluate models for predicting students' final CGPA.")
 
 # st.download_button(label='Download Report',data=EDA.py)
